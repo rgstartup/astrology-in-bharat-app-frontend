d5851376d28185438f4f108098ba7e1b6bfbb2d0
rgstartup <rgstartup1@gmail.com>
2026-01-22 10:47:49 +0530

    add the planet calculator

---
+++ astrology-in-bharat-app-frontend-new-design/apps/main/app/calculator/planet-calculator/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Planet from '@/components/calculators-charts/planet'
+
+const page = () => {
+    return (
+        <>
+
+            <Planet />
+
+        </>
+
+    )
+}
+
+export default page
